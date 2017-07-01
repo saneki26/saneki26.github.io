042f73b3c4cf30f520f49a0a411954b3cd860e3d
Alex <iop0093@mail.ru>
2017-07-01 12:26:17 +0300

    php file

--- just_a_form/mail.php
+++ just_a_form/mail.php
@@ -1,12 +1,12 @@
 <?php
 
-$recepient = "saneki2105@gmail.com";
-$sitename = "saneki26.000webhostapp.com";
+$recepient = "saneki2105@gmail.com"; /* request sending to the email address */
+$sitename = "saneki26.000webhostapp.com"; /* site of the form */
 
 $name = trim($_POST["name"]);
 $tel = trim($_POST["tel"]);
 $email = trim($_POST["email"]);
 $message = "Имя: $name \nТелефон: $tel \nE-mail: $email";
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$pagetitle = "a new request from \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
